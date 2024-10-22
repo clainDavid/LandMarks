4c8d3f68801a22e6a46a953d325c45627c7cce37
David Clain <david974.clain@gmail.com>
2024-10-22 17:06:26 +0400

    first commit

---
+++ LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by David Clain on 22/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
